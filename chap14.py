baaf2cc5627e1898562755197f53e4278784d07d
Amaka Atoyebi <amaka.atoyebi@gmail.com>
2013-09-19 17:01:29 -0400

    Updates to Chapter 14

--- chap14.py
+++ chap14.py
@@ -1,16 +1,16 @@
 # This is where chapter 14 exercises go.
 import urllib
 
-
-
 zip_entry = raw_input("Please enter your zipcode:\n")
 
 zip_open = urllib.urlopen("http://uszip.com/zip/" + zip_entry)
 for line in zip_open:
-	pop = "Total population"
-	tname = zip_entry +  "is the zip code for"
-	if pop in line:
+	if "Population" in line:
 		print line
-	elif tname in line:
+	if "Longitude" in line:
 		print line	
+	if "Latitude" in line:
+		print line
+
+zip_open.close()		
 	
